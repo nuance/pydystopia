23b7f695346a84e4b5398ecb8a8975ba3e6e2796
Matt Jones <matt-biz@mhjones.org>
2009-04-26 01:44:37 -0700

    add libs

--- setup.py
+++ setup.py
@@ -3,4 +3,10 @@ from distutils.extension import Extension
 
 from Cython.Distutils import build_ext
 
-setup(cmdclass = {'build_ext': build_ext}, ext_modules = [Extension("dystopia", ["dystopia.pyx"])])
+setup(cmdclass = {'build_ext': build_ext},
+	  ext_modules = [Extension("dystopia",
+							   ["dystopia.pyx"],
+							   include_dirs = ['/usr/local/include'],
+							   libraries = ['tokyodystopia', 'tokyocabinet',
+											'z', 'bz2', 'pthread', 'm', 'c'],
+							   library_dirs = ['/usr/local/lib'])])
